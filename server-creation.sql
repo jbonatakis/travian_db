9f6c7c317170db484b3021995354ef769579204e
DMcConnell <ddmcconnell4@gmail.com>
2018-10-15 00:44:23 -0500

    Upload server-creation.sql

---
+++ server-creation.sql
@@ -0,0 +1,3 @@
+CREATE TABLE `total_table` (`playerid` int(11) DEFAULT NULL, `xCoord` int(11) DEFAULT NULL, `yCoord` int(11) DEFAULT NULL, `tID` int(11) DEFAULT NULL, `vID` int(11) DEFAULT NULL, `village` varchar(50) DEFAULT NULL, `userID` int(11) DEFAULT NULL, `player` varchar(50) DEFAULT NULL, `aID` int(11) DEFAULT NULL, `aName` varchar(50) DEFAULT NULL, `population` int(11) DEFAULT NULL, `createdTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP);
+
+ALTER TABLE total_table CONVERT TO CHARACTER SET utf8;
